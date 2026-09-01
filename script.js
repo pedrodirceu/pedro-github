cad7ceee808bd98d3c1cc06b844859c96910a369
pedrodirceu <pedro.rodrigues.ferreira14@escola.pr.gov.br>
2026-09-01 10:55:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const perguntas = [
         enunciado:"A pirataria de livros digitais (PDFs e ePubs) deve ser combatida rigidamente ou ela funciona como uma ferramenta de democratização do acesso à cultura?",
         alternativas: [
             {
-                texto: "Ler livros pequenos, de facíl entendimento e que sejam de assuntos de interesse do leitor.",
+                texto: 
                 afirmacao: "Para quem não  tem o hábito de ler, porém tem vontadede iniciar no mundo da leitura o mais indicado seria ler livros pequenos e de assuntos que sejam de interesse do leitor para despertar o interesse da leitura e tornar a mesma um habito."
             },
             {
